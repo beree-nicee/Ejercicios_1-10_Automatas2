2562ba260e6c75026913f80d25cefafb9b7b273a
beree-nicee <23030687@itcelaya.edu.mx>
2026-07-03 11:27:09 -0600

    feat: crear el archivo de prueba.txt y el Test2 para automatizar el proceso del ejercicio 4

---
+++ Ejercicio4/Test2.py
@@ -0,0 +1,28 @@
+# importa ANTLR4 para funciones, aquí activamos el enviroment 
+from antlr4 import *
+# importarlo para obtener sus metodos 
+from ExprLexer import ExprLexer
+
+import sys 
+
+# lo que obtiene es la entrada 
+input_stream = FileStream(sys.argv[1])
+# obtiene la entraada de datos, lo analiza y lo separa en tokens
+lexer = ExprLexer(input_stream)
+
+tokens = CommonTokenStream(lexer)
+# lo llenamos 
+tokens.fill()
+
+print(tokens)
+
+# contiene el arreglo de tokens, los recorre y los imprime
+for token in tokens.tokens:
+    print("Texto: " + token.text)
+    print("Línea: " + str(token.line))
+    print("Columna: " + str(token.column))
+    nombre_token = lexer.symbolicNames[token.type]
+
+    print("Tipo : " + nombre_token)
+
+    print("==========")
