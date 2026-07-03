20c5b251b24f99115cffbc152311c4bbe3d0147f
Mariana Arriaga Vazquez <23demarzomariana@gmail.com>
2026-07-03 14:03:38 -0600

    feat:Carpeta del Ejercicio7_Operaciones con sus respectivos archivos Test,Prueba,Lexer y Expr

---
+++ Ejercicio7/Test2.py
@@ -0,0 +1,21 @@
+# Importa ANTLR4 para funciones
+from antlr4 import*
+from ExprLexer import ExprLexer
+import sys
+
+# Lo que obtiene es la entrada , analiza el texto y lo separa en tokes
+input_stream = FileStream(sys.argv[1])
+
+lexer = ExprLexer(input_stream)
+# Toma los tokens que produjo el lexer y los guarda en un flujo/lista
+tokens = CommonTokenStream(lexer)
+tokens.fill()
+print(tokens)
+print(f"{'LEXEMA':<15} {'TOKEN':<15} {'TIPO':<6} {'LINEA':<6} {'COLUMNA':<8}")
+print("-" * 60)
+
+for token in tokens.tokens:
+    if token.type == Token.EOF:
+        continue
+    nombre_token = lexer.symbolicNames[token.type]
+    print(f"{token.text:<15} {nombre_token:<15} {token.type:<6} {token.line:<6} {token.column:<8}")
